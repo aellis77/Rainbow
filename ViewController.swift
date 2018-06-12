61398acc04e72e527cf9eedf1c93ccde619f66ef
Amanda Ellis <wool.neu@gmail.com>
2018-06-12 14:42:33 -0400

    Add files via upload
    Rainbow version 1.0. Changes background color when button of corresponding color selected.

---
+++ ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  Rainbow
+//
+//  Created by user141094 on 6/12/18.
+//  Copyright © 2018 user141094. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func blueButtonAction(_ sender: Any) {
+        view.backgroundColor = .blue
+    }
+    
+    @IBAction func brownButtonAction(_ sender: Any) {
+        view.backgroundColor = .brown
+    }
+
+    @IBAction func greenButtonAction(_ sender: Any) {
+        view.backgroundColor = .green
+    }
+
+    @IBAction func redButtonAction(_ sender: Any) {
+        view.backgroundColor = .red
+    }
+    
+    @IBAction func purpleButtonAction(_ sender: Any) {
+        view.backgroundColor = .purple
+    }
+
+    @IBAction func yellowButtonAction(_ sender: Any) {
+        view.backgroundColor = .yellow
+    }
+
+}
